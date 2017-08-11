48a5441696435d824875c804e5e8f70d8d68b884
kijehyum <30900252+kijehyum@users.noreply.github.com>
2017-08-11 20:57:04 +0900

    Update models.py

--- vote/models.py
+++ vote/models.py
@@ -12,7 +12,7 @@ class Ad_outdoor(models.Model):
 	name = models.CharField(max_length = 20)
 	number = models.IntegerField(default = 0)
 	result = models.IntegerField(default=0)
-	img_url = models.CharField(max_length=500)
+	# img_url = models.CharField(max_length=500)
 	# image = models.ImageField(null=True, blank=True)
 
 
@@ -20,4 +20,4 @@ class Ad_ent(models.Model):
 	name = models.CharField(max_length = 20)
 	number = models.IntegerField(default = 0)
 	result = models.IntegerField(default=0)
-	img_url = models.CharField(max_length=500)
+	# img_url = models.CharField(max_length=500)
